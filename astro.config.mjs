d405f8c205c874a43621c695bfa1f5391dd85b84
Fristi <mark@vectos.net>
2024-02-29 07:47:01 +0100

    Visual effects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify/functions";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vectos.net",
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   markdown: {
     shikiConfig: {
       theme: "github-light",
-      wrap: false,
-    },
+      wrap: false
+    }
   },
   output: "server",
-  adapter: netlify(),
+  adapter: netlify()
 });
---
+++ src/components/react/TextGenerating.tsx
@@ -0,0 +1,55 @@
+import { useEffect } from "react";
+import { motion, stagger, useAnimate } from "framer-motion";
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+ 
+export default ({
+  words,
+  className,
+}: {
+  words: string;
+  className?: string;
+}) => {
+  const [scope, animate] = useAnimate();
+  let wordsArray = words.split(" ");
+  console.log(wordsArray);
+  useEffect(() => {
+    animate(
+      "span",
+      {
+        opacity: 1,
+      },
+      {
+        duration: 2,
+        delay: stagger(0.2),
+      }
+    );
+  }, [scope.current]);
+ 
+  const renderWords = () => {
+    return (
+      <motion.span ref={scope}>
+        {wordsArray.map((word, idx) => {
+          return (
+            <motion.span
+              key={word + idx}
+              className={cn(className, "opacity-0")}
+            >
+              {word}{" "}
+            </motion.span>
+          );
+        })}
+      </motion.span>
+    );
+  };
+ 
+  return (
+    <>
+      {renderWords()}
+    </>
+  );
+};
