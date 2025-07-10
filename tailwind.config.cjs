1c6b47e8422e2add89e61d690451ec15807105fa
Fristi <mark@vectos.net>
2025-07-10 17:27:59 +0200

    Add marquee animation and update meteor component for enhanced visual effects; replace Tech component with TechMarque for improved tech stack presentation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,8 +10,13 @@ module.exports = {
     extend: {
       animation: {
         "meteor-effect": "meteor 5s linear infinite",
+        "marquee": 'marquee 25s linear infinite'
       },
       keyframes: {
+        "marquee": {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
         meteor: {
           "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
           "70%": { opacity: "1" },
